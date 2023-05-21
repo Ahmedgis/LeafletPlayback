ba8a557a693098bf97f455cc1cba475d1e78ae71
a.samy <a.samy@prog.com>
2023-05-21 16:00:12 +0300

    Add method to convert from raw data to custom geojson format

---
+++ examples/convertToGeojson.js
@@ -0,0 +1,65 @@
+var fs = require('fs');
+var dir = 'd:/ISD/projects/Simulation/repos/LeafletPlayback/examples/geojson';
+
+// fs.readdirSync('./geojson').forEach(function (file) {
+// fs.readdirSync(dir).forEach(function (file) {
+  
+  // var geoloqi = require('./geojson/' + file);
+  // var geoloqi = require(dir + '/' + file);
+  var geoloqi = require(dir + '/' + 'new-route.js');
+
+  var geojson = {
+    type: 'Feature',
+    geometry: {
+      type: 'MultiPoint',
+      coordinates: []
+    },
+    properties: {
+      time: [],
+      speed: [],
+      altitude: [],
+      // heading: [],
+      // horizontal_accuracy: [],
+      // vertical_accuracy: [],
+      // raw: []
+    },
+    bbox: []
+  };
+
+  // geojson.bbox[0] = [geoloqi.bounds.sw.longitude, geoloqi.bounds.sw.latitude];
+  // geojson.bbox[1] = [geoloqi.bounds.sw.longitude, geoloqi.bounds.ne.latitude];
+  // geojson.bbox[2] = [geoloqi.bounds.ne.longitude, geoloqi.bounds.ne.latitude];
+  // geojson.bbox[3] = [geoloqi.bounds.ne.longitude, geoloqi.bounds.sw.latitude];
+
+var geoObject = JSON.parse(geoloqi);
+
+geojson.bbox = geoloqi.bbox;
+  
+
+
+  var points = geoloqi.features;
+  var len = geoloqi.features.count();
+
+  for(var i=0;i<len;i++){
+    var point = points[i];
+
+    var pos = point.geometry.coordinates;
+    geojson.geometry.coordinates[i] = pos[0];
+
+    var prop = geojson.properties;
+    prop.time[i] = new Date(point.properties.time).getTime();
+    prop.speed[i] = 60;
+    prop.altitude[i] = point.properties.ele;
+    // prop.heading[i] = 0;
+    // prop.horizontal_accuracy[i] = 0;
+    // prop.vertical_accuracy[i] = 0;
+  }
+
+  fs.writeFile('./geojson/' + file , JSON.stringify(geojson,null,2), function(err){
+    if (err) {
+      console.log('unable to write file: ' + err);
+    } else {
+      console.log('success');
+    }
+  });
+// });
